5f68874e2c883cb248ade8f6b7873c5fc9ba0781
Marek <depandadev@gmail.com>
2023-01-29 14:11:37 +0100

    #3 Binding Models < View Models < Views

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Reservroom.Exceptions;
 using Reservroom.Models;
+using Reservroom.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -17,30 +18,11 @@ namespace Reservroom
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            Hotel hotel = new Hotel("SingletonSean Suites");
-            try
-            {
-                hotel.MakeReservation(new Reservation(
-                    new RoomID(1, 3),
-                    "SingletonSean",
-                    new DateTime(2000, 1, 3),
-                    new DateTime(2000, 1, 4)));
+            MainWindow = new MainWindow() {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
 
-                hotel.MakeReservation(new Reservation(
-                    new RoomID(2, 4),
-                    "Marek",
-                    new DateTime(2000, 1, 4),
-                    new DateTime(2000, 1, 5)));
-            }
-            catch (ReservationConflictException ex)
-            {
-
-                throw;
-            }
-
-
-            IEnumerable<Reservation> reservations = hotel.GetAllReservations();
-            
             base.OnStartup(e);
         }
     }
---
+++ ViewModels/ReservationListingViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Reservroom.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Reservroom.ViewModels;
+
+public class ReservationListingViewModel : ViewModelBase
+{
+    // Es kann nicht direkt zu Reservation gebunden werden da es kein INotifyPropertyChanged implementiert und es zu memory leaks kommen kann
+    // Deshalb wird mit ReservationViewModel gebunden | Reservation -> ReservationViewModel -> ReservationListingViewModel
+    private readonly ObservableCollection<ReservationViewModel> _reservations;
+    // IEnumarable für capsolation 
+    public IEnumerable<ReservationViewModel> Reservations => _reservations;
+    public ICommand? MakeReservationCommand { get;}
+    
+    public ReservationListingViewModel()
+    {
+        _reservations = new ObservableCollection<ReservationViewModel>();
+
+        // Hardcode reservations
+        _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(1,2), "Max", DateTime.Now, DateTime.Now.AddHours(1))));
+        _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(3, 2), "Max", DateTime.Now.AddHours(2), DateTime.Now.AddHours(3))));
+        _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(2, 4), "Max", DateTime.Now.AddHours(4), DateTime.Now.AddHours(5))));
+        _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(4, 2), "Max", DateTime.Now.AddHours(6), DateTime.Now.AddHours(7))));
+    }
+}
