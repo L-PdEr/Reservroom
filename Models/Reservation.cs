8ed6c4d900e316031d6616564c189709317847a6
Marek <depandadev@gmail.com>
2023-01-29 12:01:30 +0100

    #2 Models und Views

---
+++ Models/Reservation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservroom.Models
+{
+    public class Reservation
+    {
+        public Reservation(RoomID roomID, string userName, DateTime startTime, DateTime endTime)
+        {
+            RoomID = roomID;
+            UserName = userName;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public RoomID RoomID { get; }
+        public string UserName { get; }
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+        public TimeSpan Length => EndTime.Subtract(StartTime);
+
+        public bool Conflics(Reservation reservation)
+        {
+            if (reservation.RoomID != RoomID)
+            {
+                return false;
+            }
+            return reservation.StartTime < EndTime && StartTime < reservation.EndTime;
+        }
+    }
+}
---
+++ Models/RoomID.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservroom.Models
+{
+    public class RoomID
+    {
+        public RoomID(int roomNumber, int floorNumber)
+        {
+            RoomNumber = roomNumber;
+            FloorNumber = floorNumber;
+        }
+
+        public int RoomNumber { get; }
+        public int FloorNumber { get; }
+
+        public override string ToString()
+        {
+            return $"{RoomNumber}{FloorNumber}";
+        }
+        // für exception aber equals wird nicht benutzt nur wir override == operator
+        public override bool Equals(object? obj)
+        {
+            return obj is RoomID id &&
+                   RoomNumber == id.RoomNumber &&
+                   FloorNumber == id.FloorNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FloorNumber, RoomNumber);
+        }
+
+        public static bool operator ==(RoomID roomID1, RoomID roomID2)
+        {
+            if (roomID1 is null && roomID2 is null) // == oder != im override führt zu overflow
+            {
+                return true;
+            }
+            return !(roomID1 is null) && roomID1.Equals(roomID2);    
+        }
+
+        public static bool operator !=(RoomID roomID1, RoomID roomID2)
+        {
+            return !(roomID1 == roomID2);
+        }
+    }
+    
+}
